11cc152d0920d5ad07398485bafeaa986724a6b5
IsabellaNguyen <75392533+IsabellaNguyen@users.noreply.github.com>
2020-12-28 15:43:58 -0500

    UPDATE GameClass 2
    - Moved the clearing answers array to the top of the randomizeAnswers() method so that the array is not cleared AFTER the method
    - Added the coverItems() method to cover all the items after it is done randomizing. This is different from the pseudocode! I did not plan to make another method just for covering items, but because of the issue where the randomizeAnswers() method would not update inside the for loop, the thread in the MainUI file (see the first commit of the MainUI file) loops the randomizeAnswers() method itself. This means I had to make another method to cover the items different from the pseudocode.
    - Added the userPicksItem() method to check which item is clicked and display the icon. This is almost exactly like how the pseudocode is written.

--- GameClass.java
+++ GameClass.java
@@ -5,69 +5,83 @@
  */
 package SummativeGame;
 
-//import static SummativeGame.MainUI.images;
-//import static SummativeGame.MainUI.items;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import javax.swing.Timer;
 
 /**
  *
  * @author bella
  */
 public class GameClass {
-    ArrayList<Integer> answers = new ArrayList();
+    ArrayList<Integer> answers = new ArrayList(); //instance variables
     ArrayList<Boolean> alrClicked = new ArrayList();
     JLabel[] items;
     ImageIcon[] icons;
     
-    int numOfCorrect;
+    int numOfCorrect; //other variables
+    int correctItem=0;
+    java.net.URL backURL = MainUI.class.getResource("backIcon.png");
+    ImageIcon backIcon = new ImageIcon(backURL);
     
     public GameClass(JLabel[] items, ArrayList answers, ArrayList alrClicked, ImageIcon[] icons){
         this.items=items;
-        this.answers=answers;
+        this.answers=answers;        
         this.alrClicked=alrClicked;
         this.icons=icons;
     }
     
     public void randomizeAnswers() throws InterruptedException{
-        //for (int a=0 ; a<7 ; a++){
-            Random rand = new Random();
-            for (int i=0 ; i<items.length ; i++){
-                int ans = rand.nextInt(6);
-                if (ans==0){ //Check if the random number is the correct item (0 represents the correct item)
-                    numOfCorrect++;
-                    if (numOfCorrect>2){ //If there are already two correct items
-                        i--; 
-                        continue; //do not add another correct item into the arraylist
-                    }
+        Random rand = new Random();
+        answers.clear(); //reset arraylist
+        numOfCorrect=0;
+        for (int i=0 ; i<items.length ; i++){
+            int ans = rand.nextInt(6);
+            if (ans==correctItem){ //Check if the random number is the correct item
+                numOfCorrect++;
+                if (numOfCorrect>2){ //If there are already two correct items
+                    i--; 
+                    continue; //do not add another correct item into the arraylist
                 }
-                answers.add(ans);
             }
-            if (numOfCorrect<2){ //does this even work 
-                    int place=rand.nextInt(30);
-                    answers.set(place, 0);
-                }
-
-            for(int i=0;i<items.length;i++){
-                items[i].setText("");
-                items[i].setIcon(icons[answers.get(i)]);
-                
-            }
-            //Thread.sleep(1000);
-            
-            System.out.println(answers);
-            answers.clear();
-            numOfCorrect=0;
-            System.out.println(answers);
-            
+            answers.add(ans);
         }
-        
-        
-    //}
+        if (numOfCorrect<2){ //does this even work 
+                int place=rand.nextInt(30);
+                answers.set(place, 0);
+            }
+
+        for(int i=0;i<items.length;i++){ //display the answers
+            items[i].setText("");
+            items[i].setIcon(icons[answers.get(i)]);
+
+        }
+            
+    }
+    
+    public void coverItems(){ //Cover items with square
+        for (JLabel item : items) {
+            item.setIcon(backIcon);
+        }
+    }
+    
+    public void userPicksItem(){ //determine which item is clicked
+        for (int i=0 ; i<items.length ; i++){ //scan each item
+            final int p=i;
+            items[i].addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if (alrClicked.get(p)==false){ //check if the item has been clicked before
+                        alrClicked.set(p, true);
+                        items[p].setIcon(icons[answers.get(p)]); //display clicked icon
+                        System.out.println(p); //TESTING
+                    }
+                    
+                }
+            });
+        }
+    }
 }
