250747634e5a9f9e4d711c82a488066655364d1b
IsabellaNguyen <75392533+IsabellaNguyen@users.noreply.github.com>
2020-12-30 20:50:26 -0500

    UPDATE PowerUpClass 2
    - I moved the mouse clicking action to this class instead of keeping it in the MainUI class. This is because 1) I wanted all the code to do with the power up in this class so it was easier to read the main class and minimalize code in the main class, 2) I didn't like how I had to initialize a few arrays AGAIN in the MainUI class to call this method properly (shown in UPDATE MainUI 3) and 3) this way the code is back to following the original pseudocode I wrote.
    - In line 40 and 46 the getter method from the GameClass is being invoked to pass the usedPowerUp variable to the GameClass so it can then pass it to the EndingPage (More detail in UPDATE GameClass 4)

--- PowerUpClass.java
+++ PowerUpClass.java
@@ -6,6 +6,8 @@
 package SummativeGame;
 
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.ImageIcon;
@@ -19,7 +21,7 @@ import javax.swing.JLabel;
 public class PowerUpClass {
     private ArrayList<Integer> answers = new ArrayList(); //instance variables
     private ArrayList<Boolean> alrClicked = new ArrayList();
-    private JButton powerUp;
+    private final JButton powerUp;
     private final JLabel[] items;
     private final ImageIcon[] icons;
     
@@ -35,24 +37,32 @@ public class PowerUpClass {
     }
     
     public void usePowerUp(){
-        //if (!usedPowerUp){
-            //usedPowerUp=true;
-            powerUp.setBackground(Color.red); //change background so user knows they've used the powerup already
-            int min=3;
-            int max=5;
-            int randnum = (int)(Math.random() * (max - min + 1) + min); //pick random amount of items to uncover
-            System.out.println(randnum); //TESTING DELETE LATER
-            for (int i=0 ; i<randnum ; i++ ){
-                Random rand = new Random(); 
-                int randItem=rand.nextInt(30); //pick random item out of the 30 available
-                if (alrClicked.get(randItem) || answers.get(randItem)==correctItem){ //if the random item has been clicked or not is the correct item
-                    i--; //pick another number
+        GameClass.getUsedPowerUp(usedPowerUp); //Reset after each game
+            powerUp.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    if (!usedPowerUp){
+                        usedPowerUp=true;
+                        GameClass.getUsedPowerUp(usedPowerUp);
+                        powerUp.setBackground(Color.red); //change background so user knows they've used the powerup already
+                        int min=3;
+                        int max=5;
+                        int randnum = (int)(Math.random() * (max - min + 1) + min); //pick random amount of items to uncover
+                        System.out.println(randnum); //TESTING DELETE LATER
+                        for (int i=0 ; i<randnum ; i++ ){
+                            Random rand = new Random(); 
+                            int randItem=rand.nextInt(30); //pick random item out of the 30 available
+                            if (alrClicked.get(randItem) || answers.get(randItem)==correctItem){ //if the random item has been clicked or not is the correct item
+                                i--; //pick another number
+                            }
+                            else{
+                                items[randItem].setIcon(icons[answers.get(randItem)]); //set icon
+                                alrClicked.set(randItem,true);
+                            }
+                        }
+                    }
                 }
-                else{
-                    items[randItem].setIcon(icons[answers.get(randItem)]); //set icon
-                    alrClicked.set(randItem,true);
-                }
-            }
-        }
-    //}
+            });
+    }
+    
 }
