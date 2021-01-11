5ed3888d1395cfedbcbcf881091f0f0b61de621a
IsabellaNguyen <75392533+IsabellaNguyen@users.noreply.github.com>
2021-01-10 21:31:56 -0500

    UPDATE GameClass 6
    - Added the present special item, which is an item that adds one point. Went well according to the pseudocode for the special item.
    - Added an evil special item, which is an item that covers up everything again. This went according to the pseudocode except that I needed to add a timer before the items are covered again or else you can't see what happens. (timer in UPDATE MainUI 6)
    - Added settings. This went according to the pseudocode for the most part, but there are still issues with changing JFrames in the middle of the game that will have to be fixed later on.
    - I added a startedGame() method to set the icons when the user comes back from the instructions page (if they clicked the help button in settings) instead of putting it in the MainUI class.

--- GameClass.java
+++ GameClass.java
@@ -7,8 +7,16 @@ package SummativeGame;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
@@ -27,18 +35,23 @@ public class GameClass {
     private final JFrame mainFrame;
     private final JLabel[] stars;
     private final JLabel text;
+    private final JLabel extraStar;
     
-    int numOfCorrect; //other variables
+    int numOfCorrect, numOfPresent, numOfEvil; //other variables
     int correctItem=0;
     int correct=0;
     int points=5;
-    static boolean used=false;
+    int present=6;
+    int evil=7;
+    static boolean used,isEvil=false;
     java.net.URL backURL = GameClass.class.getResource("ImageOnBackk.png");
     ImageIcon backIcon = new ImageIcon(backURL);
     java.net.URL noStarURL = GameClass.class.getResource("noStarImage.png");
     ImageIcon noStarImage = new ImageIcon(noStarURL);
+    java.net.URL starURL = GameClass.class.getResource("star.png");
+    ImageIcon star = new ImageIcon(starURL);
     
-    public GameClass(JLabel[] items, ArrayList answers, ArrayList alrClicked, ImageIcon[] icons, JFrame frame, JLabel[] stars, JLabel text){
+    public GameClass(JLabel[] items, ArrayList answers, ArrayList alrClicked, ImageIcon[] icons, JFrame frame, JLabel[] stars, JLabel text, JLabel extraStar){
         this.items=items;
         this.answers=answers;        
         this.alrClicked=alrClicked;
@@ -46,19 +59,55 @@ public class GameClass {
         mainFrame=frame;
         this.stars=stars;
         this.text=text;
+        this.extraStar=extraStar;
+        
+        File file = new File("data.txt"); 
+        Scanner scan;
+        try {
+            scan = new Scanner(file);
+            while (scan.hasNextLine()){
+                String str = scan.nextLine();
+                if (str.equals("points")){
+                    points=scan.nextInt();
+                }
+                else if (str.equals("correct")){
+                    correct=scan.nextInt();
+                }
+            }
+            System.out.println(points + " " + correct);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MainUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    public void randomizeAnswers() throws InterruptedException{
+    public void randomizeAnswers() {
+        extraStar.setVisible(false);
         Random rand = new Random();
         answers.clear(); //reset arraylist
         numOfCorrect=0;
+        numOfPresent=0;
+        numOfEvil=0;
         for (int i=0 ; i<items.length ; i++){
-            int ans = rand.nextInt(6);
+            int ans = rand.nextInt(8);
             if (ans==correctItem){ //Check if the random number is the correct item
                 numOfCorrect++;
                 if (numOfCorrect>2){ //If there are already two correct items
                     i--; 
                     continue; //do not add another correct item into the arraylist
+                }
+            }
+            else if(ans==present){ //Check if the random number is the present
+                numOfPresent++;
+                if (numOfPresent>1){ //If there is already a present
+                    i--;
+                    continue; //Make sure there is only one present
+                }
+            }
+            else if (ans==evil){ //Checl if the random number is the evil item
+                numOfEvil++;
+                if (numOfEvil>1){ //Make sure there is only one
+                    i--;
+                    continue;
                 }
             }
             answers.add(ans);
@@ -73,6 +122,7 @@ public class GameClass {
             items[i].setIcon(icons[answers.get(i)]);
 
         }
+        SettingsClass.gameData(alrClicked, answers, points, correct); //in case the user clicks the help button before clicking any buttons
             
     }
     
@@ -87,11 +137,13 @@ public class GameClass {
     }
     
     public void userPicksItem(){ //determine which item is clicked
+        extraStar.setVisible(false);
         for (int i=0 ; i<items.length ; i++){ //scan each item
             final int p=i;
             items[i].addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
+                    //started=true;
                     if (alrClicked.get(p)==false){ //check if the item has been clicked before
                         alrClicked.set(p, true);
                         items[p].setIcon(icons[answers.get(p)]); //display clicked icon
@@ -99,22 +151,42 @@ public class GameClass {
                             correct++;
                             if (correct==2){
                                 text.setText("YOU FOUND BOTH OF KOYA'S EARS!");
-                                /*mainFrame.dispose(); //dispose of the main frame
-                                EndingPage endingPage = new EndingPage(points, used); //open ending page
-                                endingPage.setVisible(true);*/
                             }
                             else{
                                 text.setText("YOU FOUND ONE!");
                             }
+                        }
+                        else if (answers.get(p)==present){ //if the user chose the present
+                            text.setText("YOU FOUND THE PRESENT!");
+                            if (points==5){
+                                if (extraStar.isVisible()){ //possibility that user picks the present, then evil item, then present again
+                                    stars[points].setIcon(star);
+                                }
+                                else{
+                                    extraStar.setVisible(true);
+                                }
+                            }
+                            else{
+                                stars[points].setIcon(star);
+                            }
+                            points++;
+                        }
+                        else if (answers.get(p)==evil){ //if the user chose the evil item
+                            text.setText("OH NO! IT'S EVIL KOYA!");
+                            correct=0;
+                            for (int i=0 ; i<items.length ; i++){
+                                alrClicked.set(i,false);
+                            }
+                            isEvil=true;
+                            
+                            points--;
+                            stars[points].setIcon(noStarImage);
                         }
                         else{
                             points--;
                             stars[points].setIcon(noStarImage);
                             if (points==0){
                                 text.setText("TRY AGAIN NEXT TIME");
-                                /*mainFrame.dispose();
-                                EndingPage endingPage = new EndingPage(points,used);
-                                endingPage.setVisible(true);*/
                             }
                             else{
                                 text.setText("NOT QUITE");
@@ -123,15 +195,33 @@ public class GameClass {
                         
                     }
                     MainUI.itemClicked(correct, points);
+                    MainUI.isEvilClicked(isEvil);
+                    isEvil=false;
+                    SettingsClass.gameData(alrClicked, answers, points, correct);
+                    System.out.println(points + " points");
                 }
             });
         }
     }
     
-    public void openEnd(){
+    public void startedGame(){
+        for (int i=0 ; i<items.length ; i++){
+            items[i].setText("");
+            if (alrClicked.get(i)){
+                items[i].setIcon(icons[answers.get(i)]);
+            }
+        }
+        for (int i=5 ; i>points-1 ; i--){
+            stars[i].setIcon(noStarImage);
+        }
+        
+    }
+        
+    public void openEnd() throws IOException{
         mainFrame.dispose(); //dispose of the main frame
         EndingPage endingPage = new EndingPage(points, used); //open ending page
         endingPage.setVisible(true);
+        MainUI.started=false;
     }
     
 }
